28047e0d0b6761991d042bb69d464bf29393ce98
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-05-14 17:42:13 -0300

    Agregado de pages, estilos y componentes

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from 'react-router';
+
+import { MarvelPage, DcPage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
+
+export const AppRouter = () => {
+  return (
+    <>
+
+    <Navbar />
+    
+    <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
+
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="/" element={<Navigate to="/marvel" />} />
+
+    </Routes>
+    
+    </>
+  )
+}
