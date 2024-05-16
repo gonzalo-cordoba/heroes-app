98e7b63bf84917bb46c664988c9a0697a57b97a2
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-05-16 17:04:00 -0300

    useNavigate, Lista de heroes, Tarjetas del heroe, Leer argumentos por URL, useMemo, SearchComponent

---
+++ src/heroes/helpers/getHeroesByName.js
@@ -0,0 +1,15 @@
+import { heroes } from "../data/heroes";
+
+
+export const getHeroeByName = ( name = '' ) => {
+
+    name = name.toLowerCase().trim();
+
+    if ( name.length === 0 ) return [];
+
+    return heroes.filter(
+        hero => hero.superhero.toLocaleLowerCase().includes( name )
+    );
+
+
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,18 @@
-import { Navigate, Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router';
 
-import { MarvelPage, DcPage } from '../heroes';
+import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
-import { Navbar } from '../ui';
 
 export const AppRouter = () => {
   return (
     <>
-
-    <Navbar />
     
     <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
 
         <Route path="login" element={<LoginPage />} />
+        <Route path="/*" element={ <HeroesRoutes /> } />
 
-        <Route path="/" element={<Navigate to="/marvel" />} />
+        
 
     </Routes>
     
